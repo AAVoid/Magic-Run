11b4ece4c7781e6e2aa3c9a4b94f0adbe2ed0f99
A. A <tepoo17@hotmail.com>
2018-01-13 23:43:49 -0400

    Affichage FXML page accueil en cours

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -13,7 +13,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application { 
+public class Main extends Application {
+	public static final String NOM_APPLLICATION = "Magic Run - Aymerik ABOSO";
+	public static final int LONGUEUR_FENETRE = 600;
+	public static final int HAUTEUR_FENETRE = 600;
+	public static final String CHEMIN_FXML_PAGE_ACCUEIL = "/vue/pageAccueil.fxml";
+	public static final String CHEMIN_FICHIER_CSS = "vue/ApplicationTheme.css";
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -21,9 +26,12 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("/Sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+		Parent root = FXMLLoader.load(getClass().getResource(Main.CHEMIN_FXML_PAGE_ACCUEIL));
+        primaryStage.setTitle(Main.NOM_APPLLICATION);
+        Scene scene = new Scene(root, Main.LONGUEUR_FENETRE, Main.HAUTEUR_FENETRE);
+        scene.getStylesheets().add(Main.CHEMIN_FICHIER_CSS);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
 	}
 }
