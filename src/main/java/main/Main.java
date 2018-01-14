e0893a74e813720f793fd175291430213d109a9b
A. A <tepoo17@hotmail.com>
2018-01-13 21:22:06 -0400

    Commit initial creation projet

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,52 @@
+package main;
+
+
+
+/*
+ATEUR : Aymerik ABOSO
+ANNEE : 2018
+*/
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application { 
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/Sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
